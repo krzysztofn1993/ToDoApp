5e15a7e97940c5656054322dc599f9d5f36a499b
KrzysztofN <krzysztofn1993@gmail.com>
2019-03-12 19:14:40 +0200

    Added bootstrap

---
+++ App/Views/content/Home.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Home Page";
+require_once('../App/Views/common/head.php');
+?>
+<body>
+    <h1>Welcome to my ToDoApp</h1>
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+<?php
+require_once('../App/Views/common/footer.php');
+?>
--- App/Views/content/Login.php
+++ App/Views/content/Login.php
@@ -1,17 +1,18 @@
 <?php
 $title = "Login Page";
 require_once('../App/Views/common/head.php');
-echo "<link rel=\"stylesheet\" href=/stylesheets/main.css>";
 ?>
-</head>
 <body>
-    <form action="/app" method="POST" autocomplete="off">
-        <label for="login">Login:</label>
-        <input type="text" name="login" id="login" required autocomplete="off">
-        <label for="password">Password:</label>
-        <input type="password" name="password" required autocomplete="off">
-        <button type="submit">Log In</button>
-    </form>
+<div class="col-lg-12">
+    <h1>Hello</h1>
+</div>
+<form action="/app" method="POST" autocomplete="off">
+    <label for="login">Login:</label>
+    <input type="text" name="login" id="login" required autocomplete="off">
+    <label for="password">Password:</label>
+    <input type="password" name="password" required autocomplete="off">
+    <button type="submit">Log In</button>
+</form>
 <p>If you dont have account <a href="/register">register here</a></p>    
 <?php
 require_once('../App/Views/common/footer.php');

58b09115d129d9f5b67ddc5e526eece85995af01
KrzysztofN <krzysztofn1993@gmail.com>
2019-03-13 18:57:24 +0200

    Created register view

--- App/Views/content/Home.php
+++ App/Views/content/Home.php
@@ -5,7 +5,7 @@ require_once('../App/Views/common/head.php');
 <body class="bg-primary vh-100">
 <div class="col-lg-12 h-100 wrapper d-flex flex-column justify-content-center align-items-center">
     <div class="row">
-        <h1 class="">Welcome!<br> ToDoApp</h1>
+        <h1>Welcome!<br> ToDoApp</h1>
     </div>
     <div class="row">
         <form action="./app" method="POST" autocomplete="off">
@@ -19,11 +19,11 @@ require_once('../App/Views/common/head.php');
                 <input type="password" name="password" required autocomplete="off" 
                 placeholder="Password" class="rounded my-2">
             </div>
-    </div>
-    <div class="row d-flex justify-content-around my-2">
-        <a class="btn btn-success border border-dark text-dark mx-2" href="./login">Login</a>
+            <div class="row d-flex justify-content-around my-2">
+                <input type="submit" value="Login" class="btn btn-success border border-dark text-dark mx-2">
+                <a class="btn btn-secondary border border-dark text-dark mx-2" href="./register">Register</a>
+            </div>
         </form>
-        <a class="btn btn-secondary border border-dark text-dark mx-2" href="./register">Register</a>
     </div>
 </div>
 <?php

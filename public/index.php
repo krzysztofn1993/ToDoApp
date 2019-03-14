9e8db8f19768c5b4051972cb6f035deb84c68090
KrzysztofN <krzysztofn1993@gmail.com>
2019-03-14 18:33:52 +0200

    Revert "Revert "Created logic for creating database""
    This reverts commit 71568b4336cc5ed977b72fd6e08b940ab5b242da.
    
    Created logic for creating database or table when there is no selected
    database or table.

--- public/index.php
+++ public/index.php
@@ -3,9 +3,12 @@ session_start();
 require_once("../vendor/autoload.php");
 
 use Core\Router;
+use App\Model\Database;
 
 
-$router = new Router;
-$router->goTo($_SERVER['QUERY_STRING']);
+$db = new Database;
+
+// $router = new Router;
+// $router->goTo($_SERVER['QUERY_STRING']);
 
 ?>

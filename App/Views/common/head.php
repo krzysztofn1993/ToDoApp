eb951d40991b21646a5c498be1c6c4443c060da5
KrzysztofN <krzysztofn1993@gmail.com>
2019-03-18 18:23:52 +0200

    Added absolute paths to css files

--- App/Views/common/head.php
+++ App/Views/common/head.php
@@ -13,6 +13,6 @@
     ?>
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="stylesheets/main.css">
+    <link rel="stylesheet" type="text/css" href="/Projects/ToDoApp/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/Projects/ToDoApp/public/stylesheets/main.css">
     </head>
